45b1388d3468ce3e2ae6b26d81d23299ed61d6e7
Isaiah Desta <isaiahdesta@Isaiahs-MBP.attlocal.net>
2022-10-22 13:43:59 -0400

    work in progress.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "../components/BackgroundCircles";
+import Link from "next/link";
 
 type Props = {};
 
@@ -12,11 +13,36 @@ export default function Hero({}: Props) {
   });
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
-        <BackgroundCircles />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#F7ABOA" />
-      </h1>
+      <BackgroundCircles />
+      {/* replace the img tag below with proper Image tag || whitelist the url. */}
+      <img
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src="https://picsum.photos/600/600"
+        alt=""
+      />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Software Engineer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold scroll-px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F7ABOA" />
+        </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import About from "../components/About";
+import Experience from "../components/Experience";
 
 const Home: NextPage = () => {
   return (
@@ -10,9 +12,16 @@ const Home: NextPage = () => {
         <title>portfolio test</title>
       </Head>
       <Header />
-      <section id="hero" className="snap-center">
+      <section id="hero" className="snap-start">
         <Hero />
+      </section>
 
+      <section id="about" className="snap-center">
+        <About />
+      </section>
+
+      <section id="experience" className="snap-center">
+        <Experience />
       </section>
     </div>
   );
