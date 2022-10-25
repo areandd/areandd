15e7266b3d7e805b67047f17878eca84b288dd23
Isaiah Desta <isaiahdesta@Isaiahs-MBP.attlocal.net>
2022-10-25 07:59:45 -0400

    stable version

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import Experience from "../components/Experience";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -38,7 +39,15 @@ const Home: NextPage = () => {
       <section id="contact" className="snap-start">
         <Contact />
       </section>
-
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <img 
+            className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+            src="https://picsum.photos/400/500" alt="" />
+          </div>
+        </footer>
+      </Link>
     </div>
   );
 };
